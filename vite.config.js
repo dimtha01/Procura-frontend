b8c69bdc5962125186233d8539aa9f7228cb1644
dimtha01 <richarsonmartinez400@gmail.com>
2026-01-31 12:43:25 -0400

    This version of Antigravity is no longer supported. Please update to receive the latest features!

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 80,
-    host: true  // Opcional: expone en red local (0.0.0.0)
+    host: true // Opcional: expone en red local (0.0.0.0)
   }
 })
